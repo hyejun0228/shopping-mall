1ff14d7b450f3a49ad3e1947b5046b4a344b85f0
hyejun <hyejunkkim228@gmail.com>
2025-06-02 17:18:26 +0900

    chore: 아이콘 설정 추가 및 절대 경로 설정 수정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import svgrPlugin from 'vite-plugin-svgr';
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgrPlugin(),
+  ],
   server: {
-    port: 3000
+    port: 3000,
+    open: true,
   },
   resolve: {
     alias: {
