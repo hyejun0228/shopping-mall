a221b58db08a958d936acdbd7d0602a8619fb836
hyejun <hyejunkkim228@gmail.com>
2025-06-02 17:18:39 +0900

    feat: 헤더 디자인 변경

--- client/src/components/Header/Header.styled.ts
+++ client/src/components/Header/Header.styled.ts
@@ -6,8 +6,8 @@ export const Header = styled.header`
   top: 0;
   display: flex;
   align-items: center;
-  padding: 8px 20px;
-  justify-content: space-between;
+  padding: 8px 20px 16px 20px;
+  flex-direction: column;
   border-bottom: 1px solid #e0e0e0;
   background: #fff;
   height: 72px;
@@ -18,21 +18,29 @@ export const Header = styled.header`
 
 export const HeaderTitleWrapper = styled.div`
   display: flex;
+  justify-content: space-between;
   height: 100%;
   align-items: center;
-  justify-content: space-between;
+  width: 100%;
+`;
+
+export const HeaderItemWrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  height: 100%;
+  align-items: center;
   width: 100%;
 `;
 
 export const HeaderTitle = styled.h1`
-  ${typo['body-1-m']}
-  color: #154978;
-
+  ${typo['subtitle-1-b']}
+  color: #021730;
+  cursor: pointer;
 `;
 
 export const HeaderSubTitle = styled.h2`
   ${typo['caption-1-m']}
-  color: #154978;
+  color: #021730;
 `;
 
 export const HeaderButtonWrapper = styled.div`
@@ -42,12 +50,22 @@ export const HeaderButtonWrapper = styled.div`
 `;
 
 export const HeaderButton = styled.button`
-  ${typo['body-3-m']}
-  color: #154978;
+  ${typo['caption-1-m']}
+  color: #021730;
   background: transparent;
   border: none;
   cursor: pointer;
+
   &:hover {
-    text-decoration: underline;
+    ${typo['caption-1-b']}
+  }
+`;
+
+export const IconWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+
+  & > svg {
+    cursor: pointer;
   }
 `;
--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,19 +1,30 @@
 import * as S from "./Header.styled";
+import HamburgerIcon from '@/assets/svg/hamburger.svg?react';
+import ShoppingBagIcon from '@/assets/svg/shopping-bag.svg?react';
+import SearchIcon from '@/assets/svg/search.svg?react';
 
 function Header() {
   return (
     <S.Header>
+      <S.HeaderItemWrapper>
+        <S.HeaderButtonWrapper>
+          <S.HeaderButton>마이페이지</S.HeaderButton>
+          <S.HeaderButton>관심</S.HeaderButton>
+          <S.HeaderButton>알림</S.HeaderButton>
+          <S.HeaderButton>로그아웃</S.HeaderButton>
+        </S.HeaderButtonWrapper>
+      </S.HeaderItemWrapper>
+
       <S.HeaderTitleWrapper>
         <S.HeaderTitle>
           <strong>VIVORA</strong>
         </S.HeaderTitle>
-        {/* <S.HeaderSubTitle>
-            Figurehead <strong>Kim Hyejun</strong>
-          </S.HeaderSubTitle> */}
-        <S.HeaderButtonWrapper>
-          <S.HeaderButton>마이페이지</S.HeaderButton>
-          <S.HeaderButton>장바구니</S.HeaderButton>
-        </S.HeaderButtonWrapper>
+        <S.IconWrapper>
+            <SearchIcon />
+            <ShoppingBagIcon />
+            <HamburgerIcon />
+        </S.IconWrapper>
+
       </S.HeaderTitleWrapper>
     </S.Header>
   );
