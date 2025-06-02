27eb613695181b827da43214fda72e952f451f4d
hyejun <hyejunkkim228@gmail.com>
2025-06-02 17:34:41 +0900

    chore: 폰트 추가

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import GlobalStyles from "./styles/global";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <GlobalStyles />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
